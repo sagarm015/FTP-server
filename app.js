3a9c33848c4cd1bab350328880ae731e901565c6
Sagar mittal <19ucs182@lnmiit.ac.in>
2021-07-04 18:16:21 +0530

    second commit

--- app.js
+++ app.js
@@ -10,4 +10,4 @@ app.use(
   serveIndex('public/ftp',{icons:true})
 );
 
-app.listen(3000,()=>console.log("server started ta port 3000"));
+app.listen(process.env.PORT || 3000,()=>console.log("server started ta port 3000"));
